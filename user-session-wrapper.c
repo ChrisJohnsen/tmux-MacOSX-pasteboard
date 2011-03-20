25b03999e05c9a0f4d8feb9897729ecf92bdbecf
Chris Johnsen <chris_johnsen@pobox.com>
2011-03-20 16:51:20 -0500

    wrapper: use uname(3) to find OS version
    We warn if the OS is not "Darwin".
    
    Major versions prior to 10.2 are not fully decoded (we have no
    "attach" implementation for 10.4 and earlier anyway).

--- user-session-wrapper.c
+++ user-session-wrapper.c
@@ -1,11 +1,12 @@
 #include <string.h>    /* strlen, strcpy, strerror, strcmp, strrchr */
 #include <stdarg.h>    /* va_...   */
 #include <stdio.h>     /* fprintf, vfprintf  */
-#include <stdlib.h>    /* malloc, exit, free */
+#include <stdlib.h>    /* malloc, exit, free, atoi */
 #include <dlfcn.h>     /* dlsym    */
 #include <stdint.h>    /* uint64_t */
 #include <unistd.h>    /* execvp   */
 #include <sys/errno.h> /* errno    */
+#include <sys/utsname.h> /* uname  */
 
 #if 0
 void * _vprocmgr_move_subset_to_user(uid_t target_user, const char *session_type, uint64_t flags); /* 10.6 */
@@ -29,12 +30,40 @@ void die(int ev, const char *fmt, ...) {
     exit(ev);
 }
 
+/*
+ * ##__VA_ARGS_ is a GNU CPP extension to invoke with zero extra args.
+ * Expects f to be a literal string (for compile-time concatenation).
+ */
+#define die_errno(x,f,...) die((x),(f ": %s"),##__VA_ARGS__,strerror(errno))
+
 int main(int argc, char *argv[]) {
     if (argc < 2)
         die(1, "usage: %s <program> [args...]", argv[0]);
 
-    /* TODO: detect OS version */
-    unsigned int os = 1060;
+    unsigned int os = 0;
+
+    struct utsname u;
+    if (uname(&u))
+        die_errno(2, "uname failed");
+    if (strcmp(u.sysname, "Darwin"))
+        fprintf(stderr, "warning: unsupported OS sysname: %s\n", u.sysname);
+
+    char *rest, *whole = strdup(u.release);
+    if (!whole)
+        die_errno(2, "strdup failed");
+    rest = whole;
+    strsep(&rest, ".");
+    if (whole && *whole && whole != rest) {
+        int major = atoi(whole);
+        if (major >= 6) /* 10.2 -- 10.7 */
+            os = 1000 + (major-4) * 10;
+        else /* 10.1, 10.0 and prior betas/previews */
+            os = 1000;
+    }
+    else
+        fprintf(stderr, "warning: unparsable major release number: '%s'\n", u.release);
+
+    free(whole);
 
     if (os < 1050) {
         fprintf(stderr, "warning: unsupported old OS, trying as if it were 10.5\n");
